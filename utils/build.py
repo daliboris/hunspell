ea26228c64bf94ecc23771eecac61f559e698af8
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2014-11-02 10:02:40 +0100

    Moved code upstream to share it with other repositories

--- utils/build.py
+++ utils/build.py
@@ -172,13 +172,11 @@ You may use any of the following filtering options:
 
 import codecs, json, re, sys
 from getopt import getopt, GetoptError
-from os import listdir, makedirs, path, popen, sep, walk
-
-from PyICU import Locale, Collator
+from os import makedirs, path, sep
 
 
 def root_dir():
-    return path.join(path.dirname(__file__), "..", "..")
+    return path.join(path.dirname(__file__), "..")
 
 
 def external_dir():
@@ -186,7 +184,19 @@ def external_dir():
 
 
 sys.path.append(path.join(external_dir(), "pydiomatic"))
-from idiomatic.ui import warn_about_option_overwrite
+from idiomatic.data import filters_to_paths, load_presets
+from idiomatic.languages import collator
+from idiomatic.ui import \
+    error_and_exit_if_unsupported_language, \
+    list_languages, \
+    list_presets, \
+    load_preset_or_error_and_exit, \
+    missing_language_error_and_exit, \
+    warn_about_option_overwrite, \
+    warn_if_preset_and_filters
+
+sys.path.append(path.join(external_dir(), "lexicon"))
+from lexicon import load_word_data
 
 
 def data_dir():
@@ -197,115 +207,12 @@ def lexicon_data_dir():
     return path.join(external_dir(), "lexicon", "data")
 
 
+def data_dirs():
+    return [lexicon_data_dir(), data_dir()]
+
+
 def spelling_data_dir():
     return path.join(external_dir(), "spelling", "data")
-
-
-def lexion_extension():
-    return u".lex.json"
-
-
-def list_languages():
-    folders = []
-    for folder in listdir(data_dir()):
-        if folder.strip(".") and folder != u"common":
-            folders.append(folder)
-    for folder in sorted(folders):
-        print folder
-
-
-def load_language_data(language):
-    language_file_path = path.join(data_dir(), language, "language.json")
-    with open(language_file_path, "rb") as fp:
-        return json.load(fp)
-
-
-def language_collator(language):
-    return Collator.createInstance(Locale(language + ".UTF-8"))
-
-
-def list_presets(language):
-    presets = []
-    language_data = load_language_data(language)
-    if "presets" in language_data:
-        collator = language_collator(language)
-        for preset in sorted(language_data["presets"].keys(), cmp=collator.compare):
-            print preset
-            if "displayName" in language_data["presets"][preset]:
-                print u" "*4 + language_data["presets"][preset]["displayName"]
-            if "description" in language_data["presets"][preset]:
-                print u" "*4 + language_data["presets"][preset]["description"]
-
-
-def load_preset(name, language):
-    preset = {}
-    language_data = load_language_data(language)
-    if "presets" in language_data:
-        if name in language_data["presets"]:
-            preset = language_data["presets"][name]
-    return preset
-
-
-def iter_module_folder_paths(folder, extension):
-    for root_folder, folder_names, file_names in walk(folder):
-        for file_name in file_names:
-            if file_name.endswith(extension):
-                yield path.join(root_folder, file_name)
-
-
-def filter_to_modules(filter):
-    if isinstance(filter, list):
-        return filter
-    else:
-        return filter.split(u",")
-
-
-def iter_module_paths(module_path, extension):
-
-    # If with .<extension> it is a file, include the file.
-    # Note: if both a <module> folder and a <module>.<extension> file exist, both are yield.
-    module_file_path = u"{}.{}".format(module_path, extension)
-    if path.isfile(module_file_path):
-        yield module_file_path
-
-    # If module is a folder, get all .<extension> files inside.
-    if path.isdir(module_path):
-        for file_path in iter_module_folder_paths(module_path, extension):
-            yield file_path
-
-
-def module_to_paths(module, language, extension):
-    search_folder = language
-    if module.startswith(u"common/"):
-        search_folder = u"common"
-        module = module[7:]
-    search_paths = [path.join(data_dir(), search_folder),
-                    path.join(spelling_data_dir(), search_folder)]
-    path_batch = set()
-    for search_path in search_paths:
-        if module != u"*":
-            search_path = path.join(search_path, module)
-        for found_path in iter_module_paths(search_path, extension=extension):
-            path_batch.add(found_path)
-    return path_batch
-
-
-def filters_to_paths(filters, language, extension):
-    paths = set()
-    for filter in filters:
-        if isinstance(filter, list):
-            filter_type, filter_string = filter
-        else:
-            filter_type = "include"
-            filter_string = filter
-        module_strings = filter_to_modules(filter_string)
-        for module_string in module_strings:
-            path_batch = module_to_paths(module_string, language=language, extension=extension)
-            if filter_type == "include":
-                paths = paths | path_batch
-            else: # exclude
-                paths = paths - path_batch
-    return paths
 
 
 def strip_comments(line):
@@ -355,8 +262,7 @@ def lines_to_rep(lines, language):
     content = u""
     if lines:
         content = u"REP {}\n".format(len(lines))
-        collator = language_collator(language)
-        for line in sorted(lines, cmp=collator.compare):
+        for line in sorted(lines, cmp=collator(language).compare):
             content += u"REP {}\n".format(line)
     return content
 
@@ -379,116 +285,6 @@ def language_and_module_from_path(module_path):
     while parts[data_index] != "data":
         data_index -= 1
     return parts[data_index+1], sep.join(parts[data_index+2:])
-
-
-def load_word_data_from_file(word, file_path):
-    with open(file_path, "rb") as fp:
-        data = json.load(fp)
-    if word in data:
-        return data[word]
-    return None
-
-
-def load_word_data_from_file_if_exists(word, file_path):
-    if path.exists(file_path):
-        return load_word_data_from_file(word, file_path)
-    return None
-
-
-def load_word_data_from_folder(word, folder, skip=[]):
-    for root_folder, folder_names, file_names in walk(folder):
-        folder_names[:] = [d for d in folder_names if not
-                            any(path.join(root_folder, d).startswith(searched_path)
-                                for searched_path in skip)]
-        for file_name in file_names:
-            if file_name.endswith(lexion_extension()):
-                module_path = path.join(root_folder, file_name)
-                for searched_path in skip:
-                    if module_path.startswith(folder):
-                        continue
-                data = load_word_data_from_file_if_exists(word, module_path)
-                if data:
-                    return data
-    return None
-
-
-def load_word_model(word, language, search_from=None):
-    data_path = lexicon_data_dir()
-    searched_paths = []
-    if search_from:
-        module_path = path.join(data_path, language, search_from + lexion_extension())
-        data = load_word_data_from_file_if_exists(word, module_path)
-        if data:
-            return data
-        searched_paths.append(module_path)
-        breadcrumbs = search_from.split(sep)
-        while breadcrumbs:
-            search_path = path.join(data_path, language, sep.join(breadcrumbs))
-            data = load_word_data_from_folder(word, search_path, skip=searched_paths)
-            if data:
-                return data
-            searched_paths.append(search_path)
-            breadcrumbs = breadcrumbs[:-1]
-    search_path = path.join(data_path, language)
-    data = load_word_data_from_folder(word, search_path, skip=searched_paths)
-    if data:
-        return data
-    return None
-
-
-def root_and_suffix_from_words(words):
-    first_word = words[0]
-    root = first_word
-    first_word_suffix = u""
-    for word in words[1:]:
-        while not word.startswith(root):
-            first_word_suffix = first_word[-len(first_word_suffix)+1:]
-            root = root[:-1]
-    return root, first_word_suffix
-
-
-def apply_model_to_word(model, word, language):
-    with open(path.join(lexicon_data_dir(), language, "word-models.json")) as fp:
-        models = json.load(fp)
-    if not model in models:
-        return None
-    model_data = models[model]
-    applied_model = {
-        "common": {
-            "part-of-speech": model_data["common"]["part-of-speech"]
-        }
-    }
-    if "categories" in model_data["common"]:
-        applied_model["common"]["categories"] = model_data["common"]["categories"]
-    if "inflected" in model_data:
-        inflection = model_data["common"]["inflection"]
-        with open(path.join(lexicon_data_dir(), language, "inflections.json")) as fp:
-            inflections = json.load(fp)
-        if not inflection in inflections:
-            return None
-        inflection_data = inflections[inflection]
-        model_word_root, suffix = root_and_suffix_from_words(inflection_data)
-        inflected_data = model_data["inflected"]
-        index = 0
-        applied_model["inflections"] = []
-        while index < len(inflection_data):
-            if len(suffix):
-                inflected_word = word[:-len(suffix)] + inflection_data[index][len(model_word_root):]
-            else:
-                inflected_word = word + inflection_data[index][len(model_word_root):]
-            categories = inflected_data[index]
-            applied_model["inflections"].append([inflected_word, categories])
-            index += 1
-    else:
-        applied_model["inflections"] = [[word, []]]
-    return applied_model
-
-
-def load_word_data(word, language, search_from=None):
-    model = load_word_model(word, language=language, search_from=search_from)
-    if not model:
-        return None
-    return apply_model_to_word(model, word, language=language)
 
 
 def map_word_inflections(source_inflections, target_inflections):
@@ -539,8 +335,7 @@ def build_aff(module_paths, output_file_path, language):
 def lines_to_dic(lines, language):
     content = u""
     if lines:
-        collator = language_collator(language)
-        for line in sorted(lines, cmp=collator.compare):
+        for line in sorted(lines, cmp=collator(language).compare):
             content += line + u"\n"
     return content
 
@@ -575,7 +370,7 @@ def extend_module_paths_from_filters(module_paths, filters, language):
     for extension in module_paths.keys():
         type = extension_to_content_type(extension)
         module_paths[extension] = filters_to_paths(filters[type], language=language,
-                                                   extension=extension)
+                                                   extension=extension, search_paths=data_dirs())
     return module_paths
 
 
@@ -618,7 +413,7 @@ def main(argv):
                                             new_value=value)
             preset_name = value
         elif option == "--list-languages":
-            list_languages()
+            list_languages(data_dirs())
             sys.exit()
         elif option == "--list-presets":
             list_presets_option = True
@@ -643,15 +438,9 @@ def main(argv):
             assert False, "unhandled option"
 
     if not language:
-        print(u"Error: You did not specify a language code, " \
-              u"nor any option that does not require one.")
-        sys.exit(2)
-    elif not path.exists(path.join(data_dir(), language)):
-        print(u"Error: You specified an unsupported language code, ‘{language}’. " \
-              u"There is no subfolder in the ‘data’ folder named ‘{language}’. " \
-              u"Use the ‘--list-languages’ option to get a list of supported languages.".format(
-                  language=language))
-        sys.exit(2)
+        missing_language_error_and_exit()
+    else:
+        error_and_exit_if_unsupported_language(language, data_dirs=data_dirs())
 
     if not output_filename:
         output_filename = language
@@ -660,30 +449,20 @@ def main(argv):
         output_folder = u"build"
 
     if list_presets_option:
-        list_presets(language)
+        list_presets(language, data_dirs())
         sys.exit()
 
     if preset_name:
-        preset = load_preset(preset_name, language)
-        if not preset:
-            print(u"Error: The specified preset, ‘{}’, " \
-                  u"is not valid for language ‘{}’.".format(preset_name, language))
-            print(u"Valid presets for ‘{}’:".format(language))
-            list_presets(language)
-            sys.exit(2)
-        else:
-            if any(value for key, value in module_filters.iteritems()):
-                print(u"Warning: You have specified a preset: ‘{}’. " \
-                      u"Module filtering options are ignored.".format(preset_name))
-            module_paths = extend_module_paths_from_filters(module_paths, preset,
-                                                            language=language)
+        preset = load_preset_or_error_and_exit(preset_name, language, data_dirs())
+        warn_if_preset_and_filters(preset_name, module_filters)
+        module_paths = extend_module_paths_from_filters(module_paths, preset,
+                                                        language=language)
     else:
         if any(filters for module_extension, filters in module_filters.iteritems()):
             module_paths = extend_module_paths_from_filters(module_paths, module_filters,
                                                             language=language)
         else:
-            language_data = load_language_data(language)
-            for code, preset in language_data["presets"].iteritems():
+            for code, preset in load_presets(language, data_dirs()).iteritems():
                 if "default" in preset and preset["default"]:
                     module_paths = extend_module_paths_from_filters(module_paths, preset,
                                                                     language=language)
