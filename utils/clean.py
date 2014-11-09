979ab8ea3844422befdb6d0427d71a34d39b56cb
Adrián Chaves Fernández (Gallaecio) <adriyetichaves@gmail.com>
2014-11-09 08:29:13 +0100

    Fixed the clean script

--- utils/clean.py
+++ utils/clean.py
@@ -1,38 +1,31 @@
 # -*- coding:utf-8 -*-
 
-import os
-import sys
 import codecs
 import locale
+from os import path, walk
+import sys
 
 # See http://stackoverflow.com/a/4546129/939364
 sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 
-def getModulesSourcePath():
-    return os.path.join(
-        os.path.dirname(os.path.realpath(__file__)).decode("utf-8"),
-        u"../../src")
-
-
 # Program starts.
 
-modulesPath = getModulesSourcePath()
 fileToClean = sys.argv[1]
 inputFilesOrFolders = sys.argv[2:]
 
 entries = set()
 for inputFileOrFolder in inputFilesOrFolders:
-    if os.path.isfile(inputFileOrFolder):
+    if path.isfile(inputFileOrFolder):
         with codecs.open(inputFileOrFolder, "r", "utf-8") as fileObject:
             for line in fileObject.readlines():
                 entries.add(line.split(u" ")[0])
     else:
         for parentFolderPath, folderNames, fileNames in \
-                os.walk(inputFileOrFolder.decode("utf-8")):
+                walk(inputFileOrFolder.decode("utf-8")):
             for fileName in fileNames:
                 if fileName.endswith(u".dic"):
-                    filePath = os.path.join(parentFolderPath, fileName)
+                    filePath = path.join(parentFolderPath, fileName)
                     if filePath != fileToClean:
                         with codecs.open(filePath, "r", "utf-8") as fileObject:
                             for line in fileObject.readlines():
